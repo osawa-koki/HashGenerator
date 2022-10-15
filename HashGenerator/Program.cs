b716b72ea6ed493aae9794fa3f632ddbe819ab54
koko <github@koko-campus.com>
2022-10-16 01:43:57 +0900

    MD5完成した。

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+using (var hasher = MD5.Create())
+{
+    var md5_hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes("ABC"));
+    Console.WriteLine($"MD5 -> {BitConverter.ToString(md5_hashed).Replace("-", "")}");
+}
+
+
