7cf1c839daa476f28e3083539ed3ae89566e9d6a
koko <github@koko-campus.com>
2022-10-16 01:54:43 +0900

    対象文字列を変数化。

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -2,15 +2,16 @@
 using System.Security.Cryptography;
 using System.Text;
 
+const string target_word = "ABC";
 
 using (var hasher = MD5.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes("ABC"));
+    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
     Console.WriteLine($"MD5 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA256.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes("ABC"));
+    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
     Console.WriteLine($"SHA254 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
