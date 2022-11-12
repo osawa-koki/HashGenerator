dd64f25809ee51e8ac0d29f015155adede1cc64e
osawa-koki <github@osawa.dev>
2022-11-12 19:54:07 +0900

    コードスタイルの調整。

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -1,38 +1,34 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 const string target_word = "ABC";
 
 using (var hasher = MD5.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-    Console.WriteLine($"MD5 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
+  Console.WriteLine($"MD5 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA1.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-    Console.WriteLine($"SHA1 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
+  Console.WriteLine($"SHA1 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA256.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-    Console.WriteLine($"SHA254 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
+  Console.WriteLine($"SHA254 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA384.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-    Console.WriteLine($"SHA384 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
+  Console.WriteLine($"SHA384 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA512.Create())
 {
-    var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-    Console.WriteLine($"SHA512 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
+  Console.WriteLine($"SHA512 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
-
-
-
