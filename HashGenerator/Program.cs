2e66d64c690ac36eba44ed0d32c794032633a10a
osawa-koki <github@osawa.dev>
2022-12-01 22:49:15 +0900

    出力デザイン変えた。

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -1,18 +1,24 @@
-﻿using System.Security.Cryptography;
+using System.Security.Cryptography;
 using System.Text;
 
 const string target_word = "ABC";
 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("");
+Console.WriteLine("===== Hashing Target {0} =====", target_word);
+Console.WriteLine("");
+Console.ResetColor();
+
 using (var hasher = MD5.Create())
 {
   var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-  Console.WriteLine($"MD5 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  Console.WriteLine($"   MD5 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA1.Create())
 {
   var hashed = hasher.ComputeHash(Encoding.UTF8.GetBytes(target_word));
-  Console.WriteLine($"SHA1 -> {BitConverter.ToString(hashed).Replace("-", "")}");
+  Console.WriteLine($"  SHA1 -> {BitConverter.ToString(hashed).Replace("-", "")}");
 }
 
 using (var hasher = SHA256.Create())
